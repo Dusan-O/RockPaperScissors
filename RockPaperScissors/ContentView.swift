7c64270552e64bf39820c91ede446cc20a81af58
Dusan-O <dusan.orescanin@gmail.com>
2022-08-28 12:41:15 +0200

    Add @State + let + var for the game

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// Bool to show
+    @State var showAlert = false
+    @State var showSheet = false
+    
+    /// Timer
+    @State var timer: Timer?
+    
+    /// Game elements
+    @State var score = 0
+    @State var computerChoice = 0
+    @State var myChoice = 0
+    
+    /// Answers values
+    let icons = ["rock", "paper", "scissors"]
+    var pierreCasseCiseaux = "La pierre casse les ciseaux"
+    var ciseauxCoupePapier = "Les ciseaux coupent le papier"
+    var paperEnveloppePierre = "Le papier enveloppe la pierre"
+    var gagne = "C'est gagné!"
+    var perdu = "Perdu ! Essaye encore"
+    var draw = "Match nul"
+    var drawExplain = "Vous avez choisi le même objet"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+        }
     }
 }
 
