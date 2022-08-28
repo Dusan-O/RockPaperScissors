4cdd3563d16da8fb1ba80b417aab452715546732
Dusan-O <dusan.orescanin@gmail.com>
2022-08-28 12:32:43 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Dusan Orescanin on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
